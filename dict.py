abcf00c9db6fdee8265ef820e4220ab6f3c34a2b
temaantonov11 <temaantonov11@yandex.ru>
2024-12-18 22:18:14 +0300

    Scaled get_name() with help f-string. This function may get surname.

--- dict.py
+++ dict.py
@@ -4,40 +4,43 @@ import string
 # Эта функция будет ждать ввода до тех пор, пока пользователь не введет корректно имя (состоящее только из букв)
 
 # Функция вывода исключений в терминал для пользователя
-def Warning(what):
-    if what == 'name':
+def Warning(parametr):
+    if parametr == 'name':
         print("Некорректное имя.", "Имя должно содержать только латинские буквы, цифры и пробелы, первая буква – заглавная",  "Повторите ввод", sep = '\n')
+    elif parametr == 'surname':
+        print("Некорректная фамилия.", "Фамилия должна содержать только латинские буквы, цифры и пробелы, первая буква – заглавная", "Повторите ввод", sep='\n')
 
-
-def get_name():
+def get_name(parametr):
     
     # переменная-флаг для контроля того, правильно ли ввел имя пользователь
     correct_name = False
     # информация для пользователя 
-    print("Введите имя")
+    print(f"Введите {parametr}")
 
     # цикл, который позволяет принимать ввод, пока пользователь не введет корректные данные
     while (not correct_name):
         
         name = input()
 
+        # Проверка на пустоту строки
+        if len(name) == 0:
+            Warning(f"{parametr}")
+            continue
+
         # Проверка первой буквы в имени
         if name[0] in string.ascii_lowercase or name[0].isdigit():
-            Warning('name')
+            Warning(f"{name}")
             continue
 
         # Проверка всего слова на корректность 
         for x in name:
             if x not in string.ascii_letters and not x.isdigit():
-                Warning('name')
+                Warning(f"{parametr}")
                 break
         else:
             correct_name = True
 
     return name
-
-
-
 
 
 
